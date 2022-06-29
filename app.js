b2000f58fed6ff3cb74f7ac4f33d412e5c935156
Steve Calla <callasteven@gmail.com>
2022-06-29 17:27:41 -0600

    Add determine winner function

--- app.js
+++ app.js
@@ -18,12 +18,11 @@ function validateUserInput(userInput) {
       console.log('valid input'); // remove when complete
       let computerChoice = getRandomChoice();
       console.log('userInput = ', userInput, 'computer choice = ', computerChoice);
-      
+      determineWinner(userInput, computerChoice);
     }
 }
 
 // set random selection by the computer
-
 function getRandomChoice() {
   const gameChoices = ['r', 's', 'p'];
   let randomNumber = Math.floor(Math.random() * gameChoices.length);
@@ -34,8 +33,31 @@ function getRandomChoice() {
 }
 
 // compare user input and computer selection
+function determineWinner(userInput, computerInput) {
+  if (userInput === computerInput) {
+    // if no winner, select again
+    console.log('no winner, select again');
+    getUserInput();
+  } else if (userInput === 'r' && computerInput === 's') {
+    // declare user winner
+    window.alert('User wins = Rock wins against Scissors');
+    // count win in stats
+    // display winner stats
+  } else if (userInput === 's' && computerInput === 'p') {
+    // declare user winner
+    window.alert('User wins = Rock wins against Scissors');
+    // count win in stats
+    // display winner stats
+  } else if (userInput === 'p' && computerInput === 'r') {
+    // declare user winner
+    window.alert('User wins = Rock wins against Scissors');
+    // count win in stats
+    // display winner stats
+  } else { 
+    window.alert(`Computer Wins = ${computerInput} wins against ${userInput}`); 
+  }
+}
 
-// if no winner, select again
 
 // if winner, create count for winner / loser
 
